4b35d175c28e277a6a7e54c45b6a9be4d5ca075a
Borrego <joaohenriqueborrego@gmail.com>
2018-06-07 23:28:09 +0100

    add bash script for automatic preproc

---
+++ run_preproc.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+# Dataset root dir
+base_dir=$1
+# Output root dir
+out_dir=$2
+# Dataset chunk names
+declare -a chunks=(
+	"base"
+	"camera"
+	"chess"
+	"flat"
+	"gradient"
+	"light" 
+	"perlin")
+
+for chunk in "${chunks[@]}"
+do
+	python2 preproc.py \
+		--img_ext jpg \
+		--in_img_dir ${base_dir}/${chunk}/images \
+		--in_xml_dir ${base_dir}/${chunk}/annotations \
+		--out_csv ${out_dir}/${chunk}/test.csv \
+		--out_img_dir ${out_dir}/${chunk}/images_resize
+done
