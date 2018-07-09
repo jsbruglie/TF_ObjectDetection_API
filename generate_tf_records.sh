c93cc3b88e3a3b63d2bc1646667f94578eb7e675
Borrego <joaohenriqueborrego@gmail.com>
2018-07-09 09:54:20 +0100

    fix record filename to match run name

--- generate_tf_records.sh
+++ generate_tf_records.sh
@@ -5,19 +5,17 @@ base_dir=$1
 
 # Dataset chunk names
 declare -a chunks=(
-	"base"
-	"camera"
-	"chess"
-	"flat"
-	"gradient"
-	"light" 
-	"perlin")
+	"sim_no_camera_base"
+	"sim_no_camera_chess"
+	"sim_no_camera_flat"
+	"sim_no_camera_gradient"
+	"sim_no_camera_perlin")
 
 for chunk in "${chunks[@]}"
 do
 	python generate_tfrecord.py \
 		--data_path ${base_dir}/${chunk}/ \
 		--images_path ${base_dir}/${chunk}/images_resize \
-		--csv_path ${base_dir}/${chunk}/test.csv \
+		--csv_path ${base_dir}/${chunk}/${chunk}.csv \
 		--filename ${chunk}
 done
